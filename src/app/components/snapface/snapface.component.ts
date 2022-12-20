67b71660576b35b5ea6f08c0a7d8228f7fea12f6
raphaexl <ebatchas@student.1337.ma>
2022-12-20 13:51:50 +0100

    ngIf directive

--- src/app/components/snapface/snapface.component.ts
+++ src/app/components/snapface/snapface.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from 'src/app/models/face-snap.model';
 
 @Component({
   selector: 'app-snapface',
@@ -6,26 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./snapface.component.scss']
 })
 export class SnapfaceComponent implements OnInit {
-  title!:string;
-  description!:string;
-  createdDate!:Date;
-  snaps!:number;
-  imageUrl!:string;
+  @Input() faceSnap!: FaceSnap;
   snapped:boolean = false;
 
-
-
   ngOnInit(){
-    this.title = 'Archibald';
-    this.description = 'Mon meilleur ami depuis tout petit!';
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
   }
 
   onAddSnap()
   {
-    this.snaps = this.snapped ? this.snaps - 1 : this.snaps + 1;
+    this.faceSnap.snaps = this.snapped ? this.faceSnap.snaps - 1 : this.faceSnap.snaps + 1;
     this.snapped = !this.snapped;
   }
 }
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,24 +1,25 @@
 export class FaceSnap{
-    title:string;
-    description:string;
-    createdDate:Date;
-    snaps:number;
-    imageUrl:string;
-
-    constructor(title:string, description:string, imageUrl:string, createdDate:Date, snaps:number){
+    title!:string;
+    description!:string;
+    createdDate!:Date;
+    snaps!:number;
+    imageUrl!:string;
+    location?: string;
+    /*constructor(title:string, description:string, imageUrl:string, createdDate:Date, snaps:number){
         this.title = title;
         this.description= description;
         this.imageUrl = imageUrl;
         this.createdDate = createdDate;
         this.snaps = snaps;
     }
-
-    /*constructor(public title:string,
+    */
+    /*
+    constructor(public title:string,
         public description:string,
         public imageUrl: string,
-        pubic createdDate: Date,
-        public snaps: number){
-
-        }*/
-    
+        public createdDate: Date,
+        public snaps: number,
+        public location?: string){
+        }
+    */
 }
